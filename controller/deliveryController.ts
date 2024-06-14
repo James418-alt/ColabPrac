20691fb47209f19256a268521e6f7157fd93263a
James418-alt <ebifegha123@gmail.com>
2024-06-14 17:12:56 +0100

    crud completed on delivery guys

--- controller/deliveryController.ts
+++ controller/deliveryController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, request } from "express";
 import deliveryModel from "../model/deliveryGuys";
 
 export const createDeliveryGuy = async (req: Request, res: Response) => {
@@ -15,6 +15,39 @@ export const createDeliveryGuy = async (req: Request, res: Response) => {
       message: "Error Occured",
       status: 400,
       error: error.message,
+    });
+  }
+};
+
+export const viewAllDg = async (req: Request, res: Response) => {
+  try {
+    const getD = await deliveryModel.find();
+    res.status(200).json({
+      messgage: "Users List",
+      data: getD,
+      status: 200,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error Occured!",
+      status: 400,
+    });
+  }
+};
+
+export const deleteDg = async (req: Request, res: Response) => {
+  try {
+    const { Id } = req.params;
+    const getD = await deliveryModel.findByIdAndDelete(Id);
+    res.status(200).json({
+      message: "DeliveryGuy Deleted",
+      data: getD,
+      status: 200,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error Occured!",
+      data: 400,
     });
   }
 };
--- router/drouter.ts
+++ router/drouter.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { createDeliveryGuy } from "../controller/deliveryController";
+import {
+  deleteDg,
+  createDeliveryGuy,
+  viewAllDg,
+} from "../controller/deliveryController";
 
 const drouter = Router();
-drouter.route("/create-delivery").post(createDeliveryGuy);
+drouter.route("/create-dg").post(createDeliveryGuy);
+drouter.route("/view-dg").get(viewAllDg);
+drouter.route("/delete-dg/:Id").post(deleteDg);
 
 export default drouter;
