05c11dd6f34315a9917e610f49437ca72c036046
James418-alt <ebifegha123@gmail.com>
2024-06-14 16:57:43 +0100

    deliveryguys interface created

--- mainApp.ts
+++ mainApp.ts
@@ -1,5 +1,6 @@
 import { Application, Request, Response } from "express";
 import router from "./router/router";
+// import drouter from "./router/deliveryrouter";s
 
 export const mainApp = (app: Application) => {
   app.use("/api", router);
---
+++ utils/deliveyInterface.ts
@@ -0,0 +1,6 @@
+export interface iDelivery {
+  Fname: string;
+  Lname: string;
+  email: string;
+  phone: string;
+}
